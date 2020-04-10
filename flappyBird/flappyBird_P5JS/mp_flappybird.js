296c7a5b267e4fef11b0dc14d9519f1eb844b842
Mike Poirier <mpoirier@MVP.local>
2020-04-10 10:15:37 +0400

    flappy_p5js

---
+++ flappyBird/flappyBird_P5JS/mp_flappybird.js
@@ -0,0 +1,9 @@
+function setup() {
+    //createCanvas(windowWidth, windowHeight);
+    createCanvas(800, 600);
+}
+
+function draw() {
+    //green background (R, G, B)
+    background(0, 200, 0)
+}
